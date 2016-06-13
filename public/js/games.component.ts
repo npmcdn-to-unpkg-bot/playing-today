401b06f285f80938f54a5c3dd2bb6b5d3bc00e7b
jmunsch <john.munsch@aptitude.com>
2016-06-13 14:17:26 -0500

    Removed the unused Bootstrap files and started updating the games component.

--- public/js/games.component.ts
+++ public/js/games.component.ts
@@ -4,6 +4,21 @@ angular.module('Playing').component('games', {
     highlightCount: '<'
   },
   template: `
+    <div>
+      <!-- Nav tabs -->
+      <ul class="nav nav-tabs" role="tablist">
+        <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Home</a></li>
+        <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Profile</a></li>
+      </ul>
+
+      <!-- Tab panes -->
+      <div class="tab-content">
+        <div role="tabpanel" class="tab-pane active" id="home">
+        </div>
+        <div role="tabpanel" class="tab-pane" id="profile">
+        </div>
+      </div>
+    </div>
     <table class="table table-striped">
       <thead>
         <tr>
